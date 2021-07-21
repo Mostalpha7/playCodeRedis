021b062c62398349e184c716b16b15b376dd5bd3
Mustapha Abdulmujeeb <mostalpha7@gmail.com>
2021-07-21 16:20:41 +0100

    redis customization

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -6,7 +6,8 @@ const client = redis.createClient(redisUrl);
 const util = require("util");
 client.get = util.promisify(client.get);
 
-const { UserSchema, BlogSchema } = require("../models");
+const cleanCache = require("../middlewares/cleanCache");
+const { BlogSchema } = require("../models");
 
 router.get("/:id", async(req, res) => {
     const blog = await BlogSchema.findOne({
@@ -21,11 +22,13 @@ router.get("/:id", async(req, res) => {
 });
 
 router.get("/", async(req, res) => {
-    const blogs = await BlogSchema.find({ _user: req.user.id });
+    const blogs = await BlogSchema.find({ _user: req.user.id }).cache({
+        key: req.user.id,
+    });
     return res.send(blogs);
 });
 
-router.post("/", async(req, res) => {
+router.post("/", cleanCache, async(req, res) => {
     const { title, content } = req.body;
 
     const blog = new BlogSchema({
--- services/cache.js
+++ services/cache.js
@@ -3,35 +3,51 @@ const redis = require("redis");
 const redisUrl = "redis://127.0.0.1:6379";
 const client = redis.createClient(redisUrl);
 const util = require("util");
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 
 const exec = mongoose.Query.prototype.exec;
 
+mongoose.Query.prototype.cache = function(options = {}) {
+    this._useCache = true;
+    this._haskKey = JSON.stringify(options.key || "defaultKey");
+    return this;
+};
+
 mongoose.Query.prototype.exec = async function() {
-    // add the collection name to key to use in  redis
-    const key = JSON.stringify(
-        Object.assign({}, this.getQuery(), {
-            collection: this.mongooseCollection.name,
-        })
-    );
+    // If the query should not the cache.
+    if (!this._useCache) {
+        return exec.apply(this, arguments);
+    } else {
+        // add the collection name to key to use in  redis
+        const key = JSON.stringify(
+            Object.assign({}, this.getQuery(), {
+                collection: this.mongooseCollection.name,
+            })
+        );
 
-    // chech if the data is already saved
-    const cachedValue = await client.get(key);
-    if (cachedValue) {
-        // parse the stringfy cached and convert it into the current mongoose model
-        const doc = JSON.parse(cachedValue);
+        // chech if the data is already saved
+        const cachedValue = await client.hget(this._haskKey, key);
+        if (cachedValue) {
+            // parse the stringfy cached and convert it into the current mongoose model
+            const doc = JSON.parse(cachedValue);
+            return Array.isArray(doc) ?
+                doc.map((d) => new this.model(d)) :
+                new this.model(doc);
+        }
 
-        return Array.isArray(doc) ?
-            doc.map((d) => new this.model(d)) :
-            new this.model(doc);
+        // get the data from mongodb
+        const result = await exec.apply(this, arguments);
+
+        // save it in cache
+        await client.hset(this._haskKey, key, JSON.stringify(result));
+
+        // return the result
+        return result;
     }
+};
 
-    // get the data from mongodb
-    const result = await exec.apply(this, arguments);
-
-    // save it in cache
-    client.set(key, JSON.stringify(result));
-
-    // return the result
-    return result;
+module.exports = {
+    clearHash(hashKey) {
+        client.del(JSON.stringify(hashKey));
+    },
 };
